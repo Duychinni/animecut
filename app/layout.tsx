9aa2307796ace652ea481fdb13cf2e201033fb5d
Duychinni <Duychinni95@gmail.com>
2026-07-05 13:09:41 -0700

    Make global app background dark by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}>
+      <body className="min-h-full flex flex-col bg-[#07070b] text-white">{children}</body>
     </html>
   );
 }
